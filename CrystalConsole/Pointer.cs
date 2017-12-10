607ed4bffa9877eb84452118d5451e0adced4db1
Sergey/ka <acelightnin9@gmail.com>
2017-12-10 19:37:21 +0300

    Basic methods and properties were added to Pointer class

--- CrystalConsole/Pointer.cs
+++ CrystalConsole/Pointer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace CrystalConsole
 {
-    class Pointer
+    class Pointer: IEnumerable
     {
+        private List<int> offsets;
+
+        public Pointer() { }
+
+        public Pointer(int value)
+        {
+            Add(value);
+        }
+
+        public Pointer(params int[] values)
+        {
+            AddRange(values);
+        }
+
+        private void Add(int value)
+        {
+            offsets.Add(value);
+        }
+
+        private void AddRange(params int[] values)
+        {
+            offsets.AddRange(values);
+        }
+
+        public int Count()
+        {
+            return offsets.Count();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return offsets.GetEnumerator();
+        }
     }
 }
