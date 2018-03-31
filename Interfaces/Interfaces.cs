13e0894caeb033fd539aa0c2c1a911345773af83
Ace Lightning <acelightnin9@gmail.com>
2018-03-31 23:39:44 +0300

    Added separated project which will contain all interfaces the Crystal uses

---
+++ Interfaces/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface ITable
+    {
+        string Name { get; }
+
+        string GetValue(int _value);
+
+        int? GetValue(string _char);
+    }
+}
