be285a08d0cfe52c6e073728a20fc05b6837e171
Ace Lightning <acelightnin9@gmail.com>
2018-04-01 17:19:25 +0300

    Added project with GUI

---
+++ CrystalGUI/frmMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Windows.Forms;
+
+namespace CrystalGUI
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
