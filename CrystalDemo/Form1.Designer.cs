fcf06e28b899f5803f28c2148b02b3182b2cd21a
Sergey/ka <acelightnin9@gmail.com>
2018-04-16 13:12:51 +0300

    New text box control was added, all space char is being replacing to '·'

--- CrystalDemo/Form1.Designer.cs
+++ CrystalDemo/Form1.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -37,21 +39,25 @@
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeProject = new System.Windows.Forms.TreeView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.txtOriginal = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.txtNew = new FastColoredTextBoxNS.FastColoredTextBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtNewSIze = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtOriginalSize = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNew)).BeginInit();
             this.groupBox4.SuspendLayout();
             this.groupBox5.SuspendLayout();
             this.SuspendLayout();
@@ -62,7 +68,7 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(815, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(947, 24);
             this.menuStrip1.TabIndex = 8;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -82,42 +88,42 @@
             // newProjectToolStripMenuItem
             // 
             this.newProjectToolStripMenuItem.Name = "newProjectToolStripMenuItem";
-            this.newProjectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newProjectToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
             this.newProjectToolStripMenuItem.Text = "New Project...";
             this.newProjectToolStripMenuItem.Click += new System.EventHandler(this.newProjectToolStripMenuItem_Click);
             // 
             // openProjectToolStripMenuItem
             // 
             this.openProjectToolStripMenuItem.Name = "openProjectToolStripMenuItem";
-            this.openProjectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openProjectToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
             this.openProjectToolStripMenuItem.Text = "Open Project";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
             this.saveToolStripMenuItem.Text = "Save Project";
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
             this.saveAsToolStripMenuItem.Text = "Save Project as...";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(158, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Controls.Add(this.treeProject);
             this.groupBox1.Location = new System.Drawing.Point(7, 27);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 212);
@@ -125,119 +131,190 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Project Explorer";
             // 
-            // treeView1
+            // treeProject
             // 
-            this.treeView1.Location = new System.Drawing.Point(6, 19);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(188, 185);
-            this.treeView1.TabIndex = 1;
-            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeProject.Location = new System.Drawing.Point(6, 19);
+            this.treeProject.Name = "treeProject";
+            this.treeProject.Size = new System.Drawing.Size(188, 185);
+            this.treeProject.TabIndex = 1;
+            this.treeProject.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // groupBox2
             // 
             this.groupBox2.Location = new System.Drawing.Point(7, 245);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 206);
+            this.groupBox2.Size = new System.Drawing.Size(200, 257);
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Properties";
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.richTextBox2);
-            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Controls.Add(this.txtOriginal);
+            this.groupBox3.Controls.Add(this.txtNew);
             this.groupBox3.Location = new System.Drawing.Point(213, 27);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(367, 296);
+            this.groupBox3.Size = new System.Drawing.Size(500, 347);
             this.groupBox3.TabIndex = 11;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Text";
             // 
-            // richTextBox1
+            // txtOriginal
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(6, 19);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(355, 132);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.txtOriginal.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.txtOriginal.AutoScrollMinSize = new System.Drawing.Size(0, 14);
+            this.txtOriginal.BackBrush = null;
+            this.txtOriginal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtOriginal.CharHeight = 14;
+            this.txtOriginal.CharWidth = 8;
+            this.txtOriginal.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtOriginal.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.txtOriginal.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.txtOriginal.IsReplaceMode = false;
+            this.txtOriginal.Location = new System.Drawing.Point(7, 19);
+            this.txtOriginal.Name = "txtOriginal";
+            this.txtOriginal.Paddings = new System.Windows.Forms.Padding(0);
+            this.txtOriginal.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtOriginal.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("txtOriginal.ServiceColors")));
+            this.txtOriginal.Size = new System.Drawing.Size(487, 158);
+            this.txtOriginal.TabIndex = 4;
+            this.txtOriginal.WordWrap = true;
+            this.txtOriginal.Zoom = 100;
+            this.txtOriginal.TextChanging += new System.EventHandler<FastColoredTextBoxNS.TextChangingEventArgs>(this.txtOriginal_TextChanging);
+            this.txtOriginal.Load += new System.EventHandler(this.txtOriginal_Load);
+            this.txtOriginal.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyUp);
+            this.txtOriginal.MouseMove += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseMove);
+            this.txtOriginal.MouseUp += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseUp);
             // 
-            // richTextBox2
+            // txtNew
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(6, 157);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(355, 132);
-            this.richTextBox2.TabIndex = 3;
-            this.richTextBox2.Text = "";
+            this.txtNew.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.txtNew.AutoScrollMinSize = new System.Drawing.Size(0, 14);
+            this.txtNew.BackBrush = null;
+            this.txtNew.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtNew.CharHeight = 14;
+            this.txtNew.CharWidth = 8;
+            this.txtNew.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNew.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.txtNew.IsReplaceMode = false;
+            this.txtNew.Location = new System.Drawing.Point(7, 183);
+            this.txtNew.Name = "txtNew";
+            this.txtNew.Paddings = new System.Windows.Forms.Padding(0);
+            this.txtNew.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtNew.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("txtNew.ServiceColors")));
+            this.txtNew.Size = new System.Drawing.Size(487, 158);
+            this.txtNew.TabIndex = 3;
+            this.txtNew.WordWrap = true;
+            this.txtNew.Zoom = 100;
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.button1);
-            this.groupBox4.Location = new System.Drawing.Point(213, 329);
+            this.groupBox4.Controls.Add(this.btnImport);
+            this.groupBox4.Location = new System.Drawing.Point(213, 380);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(367, 122);
+            this.groupBox4.Size = new System.Drawing.Size(500, 122);
             this.groupBox4.TabIndex = 12;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Actions";
             // 
-            // button1
+            // btnImport
             // 
-            this.button1.Location = new System.Drawing.Point(6, 19);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Import Text";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnImport.Location = new System.Drawing.Point(6, 19);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(82, 23);
+            this.btnImport.TabIndex = 4;
+            this.btnImport.Text = "Import Text";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
             // 
             // groupBox5
             // 
+            this.groupBox5.Controls.Add(this.label3);
+            this.groupBox5.Controls.Add(this.txtLength);
             this.groupBox5.Controls.Add(this.label2);
-            this.groupBox5.Controls.Add(this.textBox2);
+            this.groupBox5.Controls.Add(this.txtNewSIze);
             this.groupBox5.Controls.Add(this.label1);
-            this.groupBox5.Controls.Add(this.textBox1);
-            this.groupBox5.Location = new System.Drawing.Point(586, 27);
+            this.groupBox5.Controls.Add(this.txtOriginalSize);
+            this.groupBox5.Location = new System.Drawing.Point(719, 27);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(222, 424);
+            this.groupBox5.Size = new System.Drawing.Size(222, 475);
             this.groupBox5.TabIndex = 13;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Information";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Length";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(75, 19);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(141, 20);
+            this.txtLength.TabIndex = 12;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Location = new System.Drawing.Point(6, 74);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 11;
             this.label2.Text = "New Size";
             // 
-            // textBox2
+            // txtNewSIze
             // 
-            this.textBox2.Location = new System.Drawing.Point(75, 45);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(141, 20);
-            this.textBox2.TabIndex = 10;
+            this.txtNewSIze.Location = new System.Drawing.Point(75, 71);
+            this.txtNewSIze.Name = "txtNewSIze";
+            this.txtNewSIze.Size = new System.Drawing.Size(141, 20);
+            this.txtNewSIze.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Location = new System.Drawing.Point(6, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Original Size";
             // 
-            // textBox1
+            // txtOriginalSize
             // 
-            this.textBox1.Location = new System.Drawing.Point(75, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(141, 20);
-            this.textBox1.TabIndex = 8;
+            this.txtOriginalSize.Location = new System.Drawing.Point(75, 45);
+            this.txtOriginalSize.Name = "txtOriginalSize";
+            this.txtOriginalSize.Size = new System.Drawing.Size(141, 20);
+            this.txtOriginalSize.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(815, 456);
+            this.ClientSize = new System.Drawing.Size(947, 507);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
@@ -252,6 +329,8 @@
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNew)).EndInit();
             this.groupBox4.ResumeLayout(false);
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
@@ -270,18 +349,20 @@
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeProject;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnImport;
         private System.Windows.Forms.GroupBox groupBox5;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtNewSIze;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtOriginalSize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLength;
+        private FastColoredTextBoxNS.FastColoredTextBox txtOriginal;
+        private FastColoredTextBoxNS.FastColoredTextBox txtNew;
     }
 }
 
--- CrystalDemo/Form1.cs
+++ CrystalDemo/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -28,12 +29,12 @@ namespace Crystal
             Program.book.pages[0].AddParagraph("Dracula (before the first form)", 0x2536A, 0x24582);
             Program.book.pages[0].AddParagraph("Dracula (before the second form)", 0x2570B, 0x245B1);
             Program.book.pages[0].AddParagraph("Dracual (after the fight)", 0x25C4D, 0x245E0);
-            Program.book.AddPage("History", @"D:\YandexDisk\Romhacking\Translations\03. Castlevania. Legends\Tables\History_screen.tbl", @"D:\YandexDisk\Romhacking\Translations\03. Castlevania. Legends\Tables\History_screen.tbl", "", "", "Basic Table", "Tile-Map Store Method", "14;20");
+            Program.book.AddPage("History", @"D:\YandexDisk\Romhacking\Translations\03. Castlevania. Legends\Tables\History_screen.tbl", @"D:\YandexDisk\Romhacking\Translations\03. Castlevania. Legends\Tables\History_screen.tbl", "", "", "Basic Table", "Tile-Map Store Method", "20;80");
             Program.book.pages[1].AddParagraph("History", 0x6292, 0x1661);
 
             TreeNode addingBook = new TreeNode(Program.book.BookName);
             addingBook.Tag = "Book";
-            treeView1.Nodes.Add(addingBook);
+            treeProject.Nodes.Add(addingBook);
 
             foreach (Page page in Program.book.pages)
             {
@@ -51,16 +52,19 @@ namespace Crystal
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            richTextBox1.Clear();
-            textBox1.Clear();
-            switch (treeView1.SelectedNode.Tag)
+            txtOriginal.Clear();
+            txtOriginalSize.Clear();
+            StringBuilder text;
+            switch (treeProject.SelectedNode.Tag)
             {
                 case "Paragraph":
-                    richTextBox1.Text = Program.book.ExportText(treeView1.SelectedNode.Parent.Index, treeView1.SelectedNode.Index);
-                    textBox1.Text = Program.book.pages[treeView1.SelectedNode.Parent.Index].paragraphs[treeView1.SelectedNode.Index].OriginalSize.ToString();
+                    text = new StringBuilder(Regex.Replace(Program.book.ExportText(treeProject.SelectedNode.Parent.Index, treeProject.SelectedNode.Index), @"[\s-[\r\n]]", "·"));
+
+                    txtOriginal.Text = text.ToString();
+                    txtOriginalSize.Text = Program.book.pages[treeProject.SelectedNode.Parent.Index].paragraphs[treeProject.SelectedNode.Index].OriginalSize.ToString();
                     break;
                 case "Page":
-                    textBox1.Text = Program.book.pages[treeView1.SelectedNode.Index].OriginalSize.ToString();
+                    txtOriginalSize.Text = Program.book.pages[treeProject.SelectedNode.Index].OriginalSize.ToString();
                     break;
                 default:
                     break;
@@ -70,14 +74,92 @@ namespace Crystal
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string text = richTextBox2.Text;
+            StringBuilder text = new StringBuilder(Regex.Replace(txtNew.Text, @"·", " "));
 
-            Program.book.ImportText(treeView1.SelectedNode.Parent.Index, treeView1.SelectedNode.Index, text);
+            Program.book.ImportText(treeProject.SelectedNode.Parent.Index, treeProject.SelectedNode.Index, text.ToString());
         }
 
         private void newProjectToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void richTextBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                txtLength.Text = AmountLetters(txtOriginal.SelectedText).ToString();
+            }
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            txtLength.Text = AmountLetters(txtOriginal.SelectedText).ToString();
+        }
+
+        public int AmountLetters(string _text)
+        {
+            int result = 0;
+            int i = 0;
+            string text = Regex.Replace(_text, @"\n+", "");
+
+            while (i < text.Length)
+            {
+                switch(text[i])
+                {
+                    case '[':
+                        result++;
+                        i++;
+                        int j = i;
+                        while ((j < text.Length) && (text[j] != ']'))
+                        {
+                            j++;
+                        }
+                        j++;
+                        i = j;
+                        break;
+                    case '{':
+                        result++;
+                        i++;
+                        int k = i;
+                        while ((k < text.Length) && (text[k] != '}'))
+                        {
+                            k++;
+                        }
+                        k++;
+                        i = k;
+                        break;
+                    default:
+                        result++;
+                        i++;
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        private void richTextBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtLength.Text = AmountLetters(txtOriginal.SelectedText).ToString();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOriginal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOriginal_TextChanging(object sender, FastColoredTextBoxNS.TextChangingEventArgs e)
+        {
+            if (e.InsertingText == " ")
+            {
+                e.InsertingText = "·";
+            }
         }
     }
 }
