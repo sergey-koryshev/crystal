a897869f13ca93dd80deb4f0b81e3dbd53abb4b7
Ace Lightning <acelightnin9@gmail.com>
2018-04-24 17:01:24 +0300

    - Added methods of calculation a new pointer

--- CrystalDemo/Pointer.cs
+++ CrystalDemo/Pointer.cs
@@ -15,7 +15,7 @@ namespace Crystal
 
         public Pointer() { }
 
-        public Pointer(int _offset, int _countBytes = 0)
+        public Pointer(int _offset, int _countBytes)
         {
             Offset = _offset;
             CountBytes = _countBytes;
@@ -29,8 +29,45 @@ namespace Crystal
             openFile.BaseStream.Seek(Offset, SeekOrigin.Begin);
             byte[] bytes = openFile.ReadBytes(CountBytes);
             openFile.Close();
-            result = BitConverter.ToInt32(bytes, 0);
+            result = ToInt(bytes);
 
+            return result;
+        }
+
+        public void SetPointer(string _pathToROM, int _originalTextOffset, int _newTextOffset)
+        {
+            int oldPointer = GetPointer(_pathToROM);
+
+            int difference = _newTextOffset - _originalTextOffset;
+            int newPointer = oldPointer + difference;
+            byte[] newBytes = BitConverter.GetBytes(newPointer);
+
+            BinaryWriter file = new BinaryWriter(File.Open(_pathToROM, FileMode.Open));
+            file.BaseStream.Seek(Offset, SeekOrigin.Begin);
+            int i = 0;
+            while (i < CountBytes)
+            {
+                file.Write(newBytes[i]);
+                i++;
+            }
+            file.Close();
+        }
+
+        private void ShiftBytes(ref int _pointer)
+        {
+            byte[] bytes = BitConverter.GetBytes(_pointer);
+            _pointer = ToInt(bytes);
+        }
+
+        private int ToInt(byte[] array)
+        {
+            int pos = 0;
+            int result = 0;
+            foreach (byte by in array)
+            {
+                result |= ((int)by) << pos;
+                pos += 8;
+            }
             return result;
         }
     }
