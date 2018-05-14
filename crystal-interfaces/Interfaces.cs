eb768f75b10b4b60d323efc4c67929e34a2cb821
Ace Lightning <acelightnin9@gmail.com>
2018-05-14 20:15:58 +0300

    Changes are about external Interfaces project

---
+++ crystal-interfaces/Interfaces.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface ITable
+    {
+        string Path { get; }
+
+        string ToString(List<byte> _intArray);
+
+        List<byte> ToHex(string _stringArray);
+    }
+
+    public interface IAbout
+    {
+        string Description { get; }
+
+        string Name { get; }
+
+        string Author { get; }
+
+        string Date { get; }
+
+        string Link { get; }
+
+        bool IsDefault { get; }
+
+        string Parameters { get; }
+
+        void About();
+    }
+
+    public interface IPointer
+    {
+        int GiveOffset();
+
+        int GivePointer();
+    }
+
+    public interface IStoreMethod
+    {
+        List<byte> GetBytes(int offset, string _pathToROM);
+
+        void InsertBytes(int offset, string _pathToROM, List<byte> sequence);
+    }
+}
